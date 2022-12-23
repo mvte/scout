71c0190a797a1c7c6a5aad8292bd65157c86894d
mvte <janmarzan11@gmail.com>
2022-12-22 22:48:40 -0500

    new tracker commands

---
+++ src/main/java/scout/commands/tracking/CheckTrackers.java
@@ -0,0 +1,61 @@
+package scout.commands.tracking;
+
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import scout.Scout;
+import scout.commands.Command;
+import scout.model.UserModel;
+import scout.model.UserModelDatabase;
+import scout.tracker.Tracker;
+
+import java.util.List;
+
+public class CheckTrackers implements Command {
+    @Override
+    public void handle(MessageReceivedEvent event, List<String> args) {
+        MessageChannel channel = event.getChannel();
+        UserModel user = UserModelDatabase.getInstance().getUser(event.getAuthor().getIdLong());
+        User jdaUser = Scout.bot.getUserById(user.getId());
+
+        EmbedBuilder eb = new EmbedBuilder()
+                .setTitle("your trackers")
+                .setThumbnail(jdaUser.getAvatarUrl())
+                .setFooter(jdaUser.getName())
+                .setTimestamp(java.time.Instant.now());
+
+        for(Tracker t : user.getTrackers()) {
+            String priceFound = t.getCurrentPrice() == -2 ? "PRICE_NOT_FOUND" : String.format("$%.2f", t.getCurrentPrice());
+            String statusMessage = String.format("[%s](%s)%n last price change: %s", priceFound, t.getUrl(), t.getLastPriceChange());
+            eb.addField(t.getItemName(), statusMessage, false);
+        }
+
+        try {
+            jdaUser.openPrivateChannel()
+                    .flatMap(privateChannel -> privateChannel.sendMessageEmbeds(eb.build()))
+                    .queue();
+            if(event.isFromGuild())
+                channel.sendMessage("your tracker list has been sent to your dms").queue();
+        } catch(NullPointerException e) {
+            channel.sendMessage("your dms are closed. please open them to receive your tracker list and other notifications from scout").queue();
+        }
+    }
+
+    @Override
+    public String getName() {
+        return "checktrackers";
+    }
+
+    @Override
+    public String getHelp() {
+        return "gives list of trackers. note: if the price says PRICE_NOT_FOUND, it means scout was unable to find the price of the item. " +
+                "this is usually because the item is either out of stock or the price is not displayed on the page. don't worry, scout will " +
+                "notify you when the price is found";
+    }
+
+    @Override
+    public List<String> getAliases() {
+        return List.of("ct");
+    }
+}
---
+++ src/main/java/scout/commands/tracking/RemoveTracker.java
@@ -0,0 +1,67 @@
+package scout.commands.tracking;
+
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import scout.commands.Command;
+import scout.model.URLType;
+import scout.model.UserModel;
+import scout.model.UserModelDatabase;
+import scout.tracker.TrackerChecker;
+import scout.tracker.TrackerFactory;
+
+import java.util.List;
+
+public class RemoveTracker implements Command {
+    @Override
+    public void handle(MessageReceivedEvent event, List<String> args) {
+        MessageChannel channel = event.getChannel();
+
+        if(args.size() != 1) {
+            channel.sendMessage("incorrect number of arguments provided").queue();
+            return;
+        }
+        if(URLType.getURLType(args.get(0)) == null) {
+            channel.sendMessage("invalid argument").queue();
+            return;
+        }
+
+        UserModel user = UserModelDatabase.getInstance().getUser(event.getAuthor().getIdLong());
+        scout.tracker.Tracker tracker = TrackerFactory.createTracker(args.get(0));
+
+        scout.tracker.Tracker realTracker;
+        if((realTracker = TrackerChecker.getInstance().getTracker(tracker)) == null) {
+            channel.sendMessage("this tracker does not exist").queue();
+            return;
+        }
+        if(!realTracker.getUsers().contains(user)) {
+            channel.sendMessage("you are not tracking this item").queue();
+            return;
+        }
+
+        realTracker.getUsers().remove(user);
+        user.getTrackers().remove(realTracker);
+        TrackerChecker.getInstance().clearEmptyTrackers();
+
+        EmbedBuilder eb = new EmbedBuilder()
+                .setTitle("successfully removed tracking request")
+                .addField(realTracker.getItemName(), "this tracker was removed from your requests", false)
+                .setThumbnail(event.getChannel().getJDA().getSelfUser().getAvatarUrl());
+
+        event.getChannel().sendMessageEmbeds(eb.build()).queue();
+    }
+
+    @Override
+    public String getName() {
+        return "removetracker";
+    }
+
+    @Override
+    public String getHelp() {
+        return "removes a tracker from your list of tracked items. \n usage: .removetracker <url>";
+    }
+
+    public List<String> getAliases() {
+        return List.of("rt");
+    }
+}
