96f35af9ca61f73d87c66f680951c60978f90d90
mvte <janmarzan11@gmail.com>
2022-12-26 22:24:42 -0500

    added admin commands (initialize is wip)

--- src/main/java/scout/commands/admin/Initialize.java
+++ src/main/java/scout/commands/admin/Initialize.java
@@ -1,4 +1,35 @@
 package scout.commands.admin;
 
-public class Initialize {
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import scout.commands.Command;
+import scout.commands.CommandCategory;
+
+import java.util.List;
+
+public class Initialize extends Command {
+
+    public Initialize(CommandCategory category) {
+        super(category);
+    }
+
+    @Override
+    public void handle(MessageReceivedEvent event, List<String> args) {
+        if(!event.getMember().hasPermission(Permission.MANAGE_SERVER)) {
+            return;
+        }
+
+
+    }
+
+    @Override
+    public String getName() {
+        return "initialize";
+    }
+
+    @Override
+    public String getHelp() {
+        return "admin only: sends messages to info channels";
+    }
+
 }
--- src/main/java/scout/commands/admin/Reload.java
+++ src/main/java/scout/commands/admin/Reload.java
@@ -1,4 +1,52 @@
 package scout.commands.admin;
 
-public class Reload {
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import scout.commands.Command;
+import scout.commands.CommandCategory;
+import scout.model.RutgersCourseDatabase;
+
+import java.util.List;
+
+public class Reload extends Command {
+
+    public Reload(CommandCategory category) {
+        super(category);
+    }
+
+    @Override
+    public void handle(MessageReceivedEvent event, List<String> args) {
+        MessageChannel channel = event.getChannel();
+
+        try {
+            assert event.getMember() != null;
+            if (!event.getMember().hasPermission(Permission.MANAGE_SERVER)) {
+                channel.sendMessage("you do not have permission to use this command").queue();
+                return;
+            }
+        } catch (AssertionError e) {
+            return;
+        }
+
+        channel.sendMessage("reloading...").queue();
+        RutgersCourseDatabase rutgersCourseDatabase = RutgersCourseDatabase.getInstance();
+
+        double time = rutgersCourseDatabase.loadFromEndpoint();
+        if(time == RutgersCourseDatabase.LOAD_FAILED) {
+            channel.sendMessage("failed to load courses").queue();
+        } else {
+            channel.sendMessage("loaded courses in " + time + " seconds").queue();
+        }
+    }
+
+    @Override
+    public String getName() {
+        return "reload";
+    }
+
+    @Override
+    public String getHelp() {
+        return "admin only: reloads the course database";
+    }
 }
