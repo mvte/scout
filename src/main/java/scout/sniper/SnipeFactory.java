aa0b09a58a44187c230399b8f3de16031043af34
mvte <janmarzan11@gmail.com>
2023-01-11 02:43:50 -0500

    debug factory methods

--- src/main/java/scout/sniper/SnipeFactory.java
+++ src/main/java/scout/sniper/SnipeFactory.java
@@ -5,24 +5,67 @@ import scout.model.URLType;
 
 public class SnipeFactory {
 
+
     public Snipe createSnipe(String urlString) {
         URLType url = URLType.getURLType(urlString);
         if(url == null)
             return null;
 
+        Snipe snipe;
         switch(url) {
             case AMAZON:
-                return new AmazonSnipe(urlString);
+                snipe = new AmazonSnipe(urlString);
+                break;
             case BEST_BUY:
-                return new BestBuySnipe(urlString);
+                snipe = new BestBuySnipe(urlString);
+                break;
             case GAMESTOP:
-                return new GameStopSnipe(urlString);
+                snipe = new GameStopSnipe(urlString);
+                break;
             case RUTGERS:
-                if(RutgersCourseDatabase.getInstance().containsSection(urlString))
-                    return new RutgersSnipe(urlString);
+                if(RutgersCourseDatabase.getInstance().containsSection(urlString)) {
+                    snipe = new RutgersSnipe(urlString);
+                    break;
+                }
             default:
                 return null;
         }
+
+        return snipe;
     }
 
+    public Snipe createSnipe(String urlString, boolean addToChecker) {
+        URLType url = URLType.getURLType(urlString);
+        if(url == null)
+            return null;
+
+        Snipe snipe;
+        switch(url) {
+            case AMAZON:
+                snipe = new AmazonSnipe(urlString);
+                break;
+            case BEST_BUY:
+                snipe = new BestBuySnipe(urlString);
+                break;
+            case GAMESTOP:
+                snipe = new GameStopSnipe(urlString);
+                break;
+            case RUTGERS:
+                if(RutgersCourseDatabase.getInstance().containsSection(urlString)) {
+                    snipe = new RutgersSnipe(urlString);
+                    break;
+                }
+            default:
+                return null;
+        }
+
+        if(!addToChecker) {
+            return snipe;
+        }
+
+        if(SnipeChecker.getInstance().addSnipe(snipe))
+            return snipe;
+        else
+            return SnipeChecker.getInstance().getSnipe(snipe);
+    }
 }
--- src/main/java/scout/tracker/TrackerFactory.java
+++ src/main/java/scout/tracker/TrackerFactory.java
@@ -10,12 +10,41 @@ public class TrackerFactory {
         if(urlType == null)
             return null;
 
+        Tracker tracker;
         switch(urlType) {
             case AMAZON:
-                return new AmazonTracker(url);
+                tracker = new AmazonTracker(url);
+                break;
             default:
                 return null;
         }
+
+        return tracker;
+    }
+
+    public static Tracker createTracker(String url, boolean addToChecker) {
+        URLType urlType = URLType.getURLType(url);
+
+        if(urlType == null)
+            return null;
+
+        Tracker tracker;
+        switch(urlType) {
+            case AMAZON:
+                tracker = new AmazonTracker(url);
+                break;
+            default:
+                return null;
+        }
+
+        if(!addToChecker) {
+            return tracker;
+        }
+
+        if(TrackerChecker.getInstance().addTracker(tracker))
+            return tracker;
+        else
+            return TrackerChecker.getInstance().getTracker(tracker);
     }
 
 }
