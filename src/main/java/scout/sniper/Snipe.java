7721442dc530afe0b5d8dd11cf9f39e0d5d828a8
mvte <janmarzan11@gmail.com>
2023-01-12 21:13:02 -0500

    important constants (supported urls and item name not found)

--- src/main/java/scout/sniper/Snipe.java
+++ src/main/java/scout/sniper/Snipe.java
@@ -5,8 +5,13 @@ import scout.model.UserModel;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.List;
 
 public abstract class Snipe implements Serializable {
+    //TODO: check out why addUser() is not being used?
+
+    public static final List<URLType> SUPPORTED_SNIPES = List.of(URLType.BEST_BUY, URLType.RUTGERS, URLType.GAMESTOP);
+    public static final String ITEM_NAME_NOT_FOUND = "item name not found";
 
     String url;
     String itemName;
